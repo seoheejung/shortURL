febf3df8f08e166f2aa33eadb74743490d6cc241
shj-pospot <shj0520@pospot.kr>
2023-09-18 02:23:48 +0900

    230918 mariadb에서 mysql로 변경

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 
 #### 💡 필요 패키지
 ```
-npm install next react react-dom typescript @types/react @types/node mariadb nanoid
+npm install typescript -g 
+npm install next react react-dom typescript @types/react @types/node mysql2 nanoid
 npm install --save-dev ts-node nodemon
 npm install --save-dev @types/react-dom
 ```
--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,15 @@
-import { createPool, Pool, PoolConnection } from 'mariadb';
+import { createPool } from 'mysql2/promise';
 
-let pool: Pool;
+let pool;
 
 function getPool() {
     if (!pool) {
         pool = createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: 3307, 
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         });
     }
 
@@ -16,7 +17,7 @@ function getPool() {
 }
 
 export async function saveUrlPair(shortUrl:string, originalUrl:string): Promise<void> {
-    const conn : PoolConnection | undefined= await getPool().getConnection();
+    const conn = await getPool().getConnection();
     try{
         await conn.query(`INSERT INTO saveURL (longURL, shortURL) VALUES (?, ?);`, [originalUrl,shortUrl]);
     } finally{
@@ -26,14 +27,14 @@ export async function saveUrlPair(shortUrl:string, originalUrl:string): Promise<
 }
 
 export async function getUrlByShortID(shortID:string): Promise<string | null>{
-    const conn : PoolConnection | undefined= await getPool().getConnection();
+    const conn = await getPool().getConnection();
     let rows;
     try{
-        rows=await conn.query('SELECT * FROM saveURL WHERE shortURL=?;', [shortID]);
+        [rows] = await conn.query('SELECT * FROM saveURL WHERE shortURL=?;', [shortID]);
     } finally{
         if(conn)
         conn.release(); 
     }
-    return rows[0]?.longURL || null;
-
+    
+	return rows[0]?.longURL || null;
 }
